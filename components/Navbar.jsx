f8ddb63c1ecdb30384f8b17e7d95443802da159f
Diplo2by <contactdarshanv@gmail.com>
2022-11-19 18:31:43 +0530

    added Navbar
    Navbar is completely responsive, functionality remaining.

---
+++ components/Navbar.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { BiMailSend } from "react-icons/bi";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/assets/android-chrome-256x256.png"
+          alt="/"
+          width="125"
+          height="50"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Project
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Launchpad
+              </li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className="absolute right-5 md:hidden">
+            <AiOutlineMenu size={20} />
+          </div>
+        </div>
+
+        <div
+          className={
+            nav ? "fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+          }
+        >
+          <div
+            className={
+              nav
+                ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                : "fixed left-[-100%] p-10 ease-in duration-300"
+            }
+          >
+            <div>
+              <div className="flex w-full items-center justify-between">
+                <Image
+                  src="/assets/android-chrome-256x256.png"
+                  alt="/"
+                  width={70}
+                  height={50}
+                />
+                <div
+                  onClick={handleNav}
+                  className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+                >
+                  <AiOutlineClose />
+                </div>
+              </div>
+              <div className="border-b border-gray-300 my-4">
+                <p className="w-[85%] md:w-[90%] py-4 font-semibold">
+                  To Infinity And Beyond!
+                </p>
+              </div>
+            </div>
+            <div className="py-4 flex flex-col">
+              <ul className="uppercase">
+                <Link href="/">
+                  <li className="py-4 text-sm">Home</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">About</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Skills</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Projects</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Launchpad</li>
+                </Link>
+              </ul>
+              <div className="pt-40">
+                <p className="uppercase tracking-widest text-[#5651ef]">
+                  Hit Me up!
+                </p>
+                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaGithub />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <BiMailSend />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedin />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaTwitter />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
