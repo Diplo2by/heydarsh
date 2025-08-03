b475b9395e93f4747bcfd160778db6fad8e3add2
Diplo2by <contactdarshanv@gmail.com>
2025-08-03 12:13:25 +0530

    Preview image and openGraph

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,26 @@ import About from "../components/About";
 import Main from "../components/Main";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import pwImg from "../public/assets/preview.png"
 
 export default function Home() {
   return (
     <div className="px-4 md:px-0">
       <Head>
-        <title>Darsh | WEB3 & ML</title>
-        <meta property="og:url" content="https://www.heydarsh.xyz/" />
+        <title>Darsh | Building Tech & Communities</title>
+        <meta property="og:url" content="https://www.heydarsh.in/" />
         <meta name="description" content="Tap here to takeoff!" key="desc" />
         <meta name="og:description" content="Tap here to takeoff!" key="desc" />
 
         <meta
           property="og:title"
-          content="Darsh | WEB3 & ML"
+          content="Darsh | Building Tech & Communities"
         />
 
         <meta
           property="og:description"
           content="Tap here to takeoff!"
         />
-        <meta property="og:image" content={'https://www.heydarsh.xyz/assets/p2updated.png'} />
+        <meta property="og:image" content={'https://www.heydarsh.in/assets/p3updated.webp'} />
         <meta property="og:type" content="website" />
 
         <link rel="icon" href="/favicon.ico" />
